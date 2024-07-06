f8e4bca104d081313c2bf14e3776755c9c2e419d
Rafael Cavalcante <rafael.cavalcante@tallos.com.br>
2024-07-06 19:25:10 -0300

    test: should return an error if any validation throws

--- tests/validation/validators/validation-composite.spec.ts
+++ tests/validation/validators/validation-composite.spec.ts
@@ -16,10 +16,25 @@ describe("ConnectMongoDatabaseValidation", () => {
     expect(sut).toBeDefined()
   })
 
+  it("should call validation with correct values", async () => {
+    const { sut, validationSpy } = makeSut()
+    const input = {teste: 'teste'}
+    await sut.validate(input)
+    expect(validationSpy.count).toBe(1)
+    expect(validationSpy.params).toEqual(input)
+  })
+
   it("should return an error if any validation fails", async () => {
     const { sut, validationSpy } = makeSut()
     validationSpy.result = new Error("Validation fails")
     const error = await sut.validate({})
     expect(error).toEqual(new Error("Validation fails"))
+  })
+
+  it("should return an error if any validation throws", async () => {
+    const { sut, validationSpy } = makeSut()
+    validationSpy.throwsError = true
+    const error = sut.validate({})
+    expect(error).resolves.toThrow(validationSpy.errorValue)
   })
 });
