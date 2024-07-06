71a426ecb8b76a30970b56b60b18357d121b131a
Rafael Cavalcante <rafael.cavalcante@tallos.com.br>
2024-07-06 16:52:50 -0300

    test: should return null if MongoHelper is connected

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,7 @@ export default [
     rules: {
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-namespace": "off",
+      "@typescript-eslint/no-unused-vars": "off"
     }
   }
 ];
--- tests/validation/validators/connect-mongo-database-validation.spec.ts
+++ tests/validation/validators/connect-mongo-database-validation.spec.ts
@@ -1,3 +1,4 @@
+import { MongoHelper } from "@/infra/db/mongodb/helpers/mongo-helper";
 import { ConnectMongoDatabaseValidation } from "@/validation/validators";
 
 const makeSut = () => {
@@ -8,5 +9,18 @@ describe("ConnectMongoDatabaseValidation", () => {
   it("should be defined", () => {
     const sut = makeSut()
     expect(sut).toBeDefined()
+  })
+
+  it("should return an error if MongoHelper is not connected", async () => {
+    const sut = makeSut()
+    const error = sut.validate({})
+    expect(error).rejects.toThrow(new Error("Mongo database not connected"))
+  })
+
+  it("should return null if MongoHelper is connected", async () => {
+    const sut = makeSut()
+    jest.spyOn(MongoHelper, "isConnected").mockReturnValueOnce(true)
+    const error = sut.validate({})
+    expect(error).resolves.toBeNull()
   })
 });
