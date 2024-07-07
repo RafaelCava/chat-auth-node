408678752deb59ff272f06d35fc2f97a8ce17724
Rafael Cavalcante <rafael.cavalcante@tallos.com.br>
2024-07-07 10:53:15 -0300

    test: validate user entity

--- .docker/entrypoints/entrypoint-dev.sh
+++ .docker/entrypoints/entrypoint-dev.sh
@@ -4,4 +4,6 @@ npm install
 
 npm run build
 
+git config --global --add safe.directory /usr/chat-auth-node/app
+
 npm run start:debug
---
+++ tests/domain/entities/user.spec.ts
@@ -0,0 +1,35 @@
+import { User } from '@/domain/entities/user';
+import { faker } from '@faker-js/faker';
+
+const makeData = () => ({
+  name: faker.person.fullName(),
+  email: faker.internet.email(),
+  password: faker.internet.password(),
+})
+
+describe("User entity", () => {
+  it("should be defined", () => {
+    const data = makeData()
+    const user = new User(data);
+    expect(user).toBeDefined();
+    expect(user.email).toBe(data.email);
+    expect(user.name).toBe(data.name);
+    expect(user.password).toBe(data.password);
+    expect(user.id).toBe("");
+    expect(user.createdAt).toBeInstanceOf(Date);
+    expect(user.updatedAt).toBeInstanceOf(Date);
+  })
+
+  it("should return user with id", () => {
+    const data = makeData()
+    const id = faker.string.uuid();
+    const user = new User(data, id);
+    expect(user).toBeDefined();
+    expect(user.email).toBe(data.email);
+    expect(user.name).toBe(data.name);
+    expect(user.password).toBe(data.password);
+    expect(user.id).toBe(id);
+    expect(user.createdAt).toBeInstanceOf(Date);
+    expect(user.updatedAt).toBeInstanceOf(Date);
+  })
+})
