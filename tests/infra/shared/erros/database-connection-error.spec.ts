c8fb8017c89764a1e10b03bf51948a9133dc5152
Rafael Cavalcante <rafael.cavalcante@tallos.com.br>
2024-07-07 11:08:23 -0300

    test: validate database connection error

---
+++ tests/infra/shared/erros/database-connection-error.spec.ts
@@ -0,0 +1,16 @@
+import { DatabaseConnectionError } from '@/infra/shared/erros';
+describe('DatabaseConnectionError', () => {
+  it('should be defined', () => {
+    const database = 'Teste Database'
+    const error = new Error('Teste Error')
+    const sut = new DatabaseConnectionError(database, error);
+    expect(sut).toBeDefined();
+  });
+
+  it('should have a correct error message', () => {
+    const database = 'Teste Database'
+    const error = new Error('Teste Error')
+    const sut = new DatabaseConnectionError(database, error);
+    expect(sut.message).toBe(`Could not connect to ${database}. Error: ${error.message}`)
+  })
+});
