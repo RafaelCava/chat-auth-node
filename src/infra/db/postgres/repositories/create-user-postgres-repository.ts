3610ae9cd7a7a9f991a7294865583be6055cd12d
Rafael Cavalcante <ra.facavalcante@hotmail.com>
2024-07-09 16:28:37 -0300

    test: should call create with correct params

--- src/infra/db/postgres/repositories/create-user-postgres-repository.ts
+++ src/infra/db/postgres/repositories/create-user-postgres-repository.ts
@@ -1,7 +1,11 @@
 import { CreateUserRepository } from "@/data/protocols";
+import { PostgresHelper } from "../helpers/postgres-helper";
 
 export class CreateUserPostgresRepository implements CreateUserRepository {
   async create (params: CreateUserRepository.Params): Promise<CreateUserRepository.Result> {
+    await PostgresHelper.client.user.create({
+      data: params
+    })
     return null
   }
 }
--- tests/infra/db/postgres/repositories/create-user-postgres-repository.spec.ts
+++ tests/infra/db/postgres/repositories/create-user-postgres-repository.spec.ts
@@ -1,11 +1,63 @@
+import MockDate from 'mockdate';
 import { CreateUserPostgresRepository } from "@/infra/db/postgres/repositories"
+import { PostgresHelper } from '@/infra/db/postgres/helpers/postgres-helper';
+import { PrismaClient } from '@prisma/client';
+import { faker } from '@faker-js/faker';
+import { User } from "@/domain/entities";
 
 const makeSut = () => {
   return new CreateUserPostgresRepository()
 }
 
+jest.mock('@prisma/client', () => {
+  return {
+    PrismaClient: jest.fn().mockImplementation(() => {
+      return {
+        user: {
+          create: jest.fn().mockResolvedValueOnce({ 
+            id: 'any_id',
+            email: 'any_email',
+            name: 'any_name',
+            password: 'any_password',
+            createdAt: new Date().toDateString(),
+            updatedAt: new Date().toDateString()
+          })
+        }
+      }
+    })
+  }
+})
+
 describe('CreateUserPostgresRepository', () => {
+  let prismaMock: PrismaClient;
+
+  beforeEach(() => {
+    MockDate.set(new Date())
+    prismaMock = new PrismaClient()
+    PostgresHelper.client = prismaMock
+  })
+
+  afterEach(() => {
+    MockDate.reset()
+    jest.clearAllMocks()
+  })
+
   it('Should be defined', () => {
     expect(makeSut()).toBeDefined()
+  })
+
+  it('Should call create with correct params', async () => {
+    const sut = makeSut()
+    const params = {
+      email: faker.internet.email(),
+      name: faker.person.fullName(),
+      password: faker.internet.password()
+    }
+    const makeUser = new User(params)
+    await sut.create(makeUser)
+    expect(PostgresHelper.client.user.create).toHaveBeenCalledWith({
+      data: makeUser
+    })
+    expect(PostgresHelper.client.user.create).toHaveBeenCalledTimes(1)
   })
 })
