aec53ebca23bd65e67fa07c2323df291aba956cc
Rafael Cavalcante <ra.facavalcante@hotmail.com>
2024-07-14 15:46:44 -0300

    test: ensure throws if FindUsersRepository throws

--- tests/data/usecases/list-all-users.spec.ts
+++ tests/data/usecases/list-all-users.spec.ts
@@ -37,4 +37,32 @@ describe('ListAllUsersUseCase', () => {
     expect(sut).toBeDefined()
     expect(findUsersRepositorySpy).toBeDefined()
   })
+
+  it('Should call FindUsersRepository with correct params', async () => {
+    const { findUsersRepositorySpy, sut } = makeSut()
+    const params = { limit: faker.number.int(), page: faker.number.int() }
+    await sut.listAll(params)
+    expect(findUsersRepositorySpy.params).toEqual({...params, projection: ['id', 'createdAt', 'email', 'name', 'updatedAt']})
+    expect(findUsersRepositorySpy.count).toEqual(1)
+  })
+
+  it('Should return a list of users on success', async () => {
+    const { findUsersRepositorySpy, sut } = makeSut()
+    const result = await sut.listAll({ limit: faker.number.int(), page: faker.number.int() })
+    expect(result).toEqual(findUsersRepositorySpy.result)
+  })
+
+  it('Should return an empty list if FindUsersRepository returns null', async () => {
+    const { findUsersRepositorySpy, sut } = makeSut()
+    findUsersRepositorySpy.returnNull = true
+    const result = await sut.listAll({ limit: faker.number.int(), page: faker.number.int() })
+    expect(result).toEqual([])
+  })
+
+  it('Should throw if FindUsersRepository throws', async () => {
+    const { findUsersRepositorySpy, sut } = makeSut()
+    findUsersRepositorySpy.returnError = true
+    const promise = sut.listAll({ limit: faker.number.int(), page: faker.number.int() })
+    expect(promise).rejects.toThrow(findUsersRepositorySpy.errorValue)
+  })
 })
