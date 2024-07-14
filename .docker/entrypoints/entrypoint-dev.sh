b2e2e843d61af25a516c12d6273c0f5810325ac3
Rafael Cavalcante <ra.facavalcante@hotmail.com>
2024-07-14 14:47:50 -0300

    test: ensure POST /users create an user

--- .docker/entrypoints/entrypoint-dev.sh
+++ .docker/entrypoints/entrypoint-dev.sh
@@ -4,6 +4,10 @@ npm install
 
 npm run build
 
+npx prisma migrate dev
+
+npx prisma generate
+
 git config --global --add safe.directory /usr/chat-auth-node/app
 
 npm run start:debug
--- tests/main/routes/users-routes.test.ts
+++ tests/main/routes/users-routes.test.ts
@@ -12,37 +12,51 @@ import { initPostgresSql } from '@/tests/infra/db/postgres/mocks/init-sql';
 describe('Users Routes', () => {
   let app: Express
   let postgresContainer: StartedPostgreSqlContainer;
+  let postgresClient: Client;
 
   beforeAll(async () => {
     postgresContainer = await new PostgreSqlContainer().start();
     process.env.DATABASE_URL = postgresContainer.getConnectionUri()
-    const client = new Client({
+    postgresClient = new Client({
       connectionString: process.env.DATABASE_URL,
     });
-    await client.connect();
-    await client.query(initPostgresSql);
-    await client.end();
+    await postgresClient.connect();
+    await postgresClient.query(initPostgresSql);
     await PostgresHelper.connect()
     MockDate.set(new Date())
     app = await setupApp()
   })
 
+  afterEach(async () => {
+    await postgresClient.query('DELETE FROM "User"')
+  })
+
   afterAll(async () => {
     await PostgresHelper.disconnect()
+    await postgresClient.end();
     await postgresContainer.stop();
     MockDate.reset()
   })
 
   describe('POST /users', () => {
     it('Should return 200 on /users', async () => {
+      const params = {
+        email: faker.internet.email(),
+        name: faker.person.fullName(),
+        password: faker.internet.password()
+      } 
       await request(app)
         .post('/api/users')
-        .send({
-          email: faker.internet.email(),
-          name: faker.person.fullName(),
-          password: faker.internet.password()
-        })
+        .send(params)
         .expect(201)
+        .expect((data) => {
+          expect(data.body).toHaveProperty('id')
+          expect(data.body.id).not.toBeNull()
+          expect(data.body).toHaveProperty('email', params.email)
+          expect(data.body).toHaveProperty('name', params.name)
+          expect(data.body).toHaveProperty('createdAt', new Date().toISOString())
+          expect(data.body).toHaveProperty('updatedAt', new Date().toISOString())
+        })
     })
   
     it('Should return 400 if no email is provided', async () => {
@@ -100,6 +114,20 @@ describe('Users Routes', () => {
           password: faker.internet.password()
         })
         .expect(500)
+    })
+
+    it('Should return 500 if user already exists', async () => {
+      const params = {
+        email: faker.internet.email(),
+        name: faker.person.fullName(),
+        password: faker.internet.password()
+      }
+      await postgresClient.query('INSERT INTO "User" (id, email, name, password, "updatedAt") VALUES ($1, $2, $3, $4, $5)', ['123123123', params.email, params.name, params.password, new Date()])
+      await request(app)
+        .post('/api/users')
+        .send(params)
+        .expect(500)
+        .expect({ error: 'User already exists' })
     })
   })
 })
