78b8c93f2998c32c435758140f979bcac9213c0a
Rafael Cavalcante <ra.facavalcante@hotmail.com>
2024-07-15 01:26:22 -0300

    chore: remove non used importation

--- src/main/routes/user-routes.ts
+++ src/main/routes/user-routes.ts
@@ -1,7 +1,6 @@
 import { Router } from 'express'
 import { adaptRoute } from '@/main/adapters'
 import { makeCreateUserController, makeListAllUsersController } from '@/main/factories/controllers'
-import { auth } from '../middlewares'
 
 export default (router: Router): void => {
   router.post('/users', adaptRoute(makeCreateUserController()))
