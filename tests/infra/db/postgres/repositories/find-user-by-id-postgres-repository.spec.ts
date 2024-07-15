a960d4c11e9f893d4573f5403af52d58e73c58f3
Rafael Cavalcante <ra.facavalcante@hotmail.com>
2024-07-15 01:14:20 -0300

    test: ensure FindUserByIdPostgresRepository call findFirst with correct params

--- tests/infra/db/postgres/repositories/find-user-by-id-postgres-repository.spec.ts
+++ tests/infra/db/postgres/repositories/find-user-by-id-postgres-repository.spec.ts
@@ -1,11 +1,54 @@
 import { FindUserByIdPostgresRepository } from '@/infra/db/postgres/repositories';
+import { PostgresHelper } from '@/infra/db/postgres/helpers/postgres-helper';
+import { PrismaClient } from '@prisma/client';
+import { faker } from '@faker-js/faker';
+
+jest.mock('@prisma/client', () => {
+  return {
+    PrismaClient: jest.fn().mockImplementation(() => {
+      return {
+        user: {
+          findFirst: jest.fn().mockResolvedValueOnce({ id: 'any_id' })
+        }
+      }
+    })
+  }
+})
+
 const makeSut = () => {
   return new FindUserByIdPostgresRepository()
 }
 
 describe('FindUserByIdPostgresRepository', () => {
+  let prismaMock: PrismaClient;
+
+  beforeEach(() => {
+    prismaMock = new PrismaClient()
+    PostgresHelper.client = prismaMock
+  })
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
   it('Should be defined', () => {
     const sut = makeSut()
     expect(sut).toBeDefined()
+  })
+
+  it('Should call findFirst with correct params', async () => {
+    const sut = makeSut()
+    const params = { id: faker.string.uuid(), projection: ['id', 'password'] as any }
+    await sut.findById(params)
+    expect(prismaMock.user.findFirst).toHaveBeenCalledWith({
+      where: {
+        id: params.id
+      },
+      select: {
+        id: true,
+        password: true
+      }
+    })
+    expect(prismaMock.user.findFirst).toHaveBeenCalledTimes(1)
   })
 })
