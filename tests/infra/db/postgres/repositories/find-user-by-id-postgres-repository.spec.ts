c66f385bfadeee1509d2b69c1815669c03a929e0
Rafael Cavalcante <ra.facavalcante@hotmail.com>
2024-07-15 01:16:41 -0300

    test: ensure FindUserByIdPostgresRepository return a user on success

--- tests/infra/db/postgres/repositories/find-user-by-id-postgres-repository.spec.ts
+++ tests/infra/db/postgres/repositories/find-user-by-id-postgres-repository.spec.ts
@@ -51,4 +51,11 @@ describe('FindUserByIdPostgresRepository', () => {
     })
     expect(prismaMock.user.findFirst).toHaveBeenCalledTimes(1)
   })
+
+  it('Should return a user on success', async () => {
+    const sut = makeSut()
+    const params = { id: faker.string.uuid(), projection: ['id', 'password'] as any }
+    const user = await sut.findById(params)
+    expect(user).toEqual({ id: 'any_id' })
+  })
 })
