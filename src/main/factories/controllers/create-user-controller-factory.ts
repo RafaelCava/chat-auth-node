2e729b05cb57223004fee91153c9f92b8db16111
Rafael Cavalcante <ra.facavalcante@hotmail.com>
2024-07-17 15:24:00 -0300

    chore: add hasherAdapter on createUserUseCase factory

--- src/main/factories/controllers/create-user-controller-factory.ts
+++ src/main/factories/controllers/create-user-controller-factory.ts
@@ -3,11 +3,14 @@ import { CreateUserController } from "@/presentation/controllers";
 import { Controller } from "@/presentation/protocols";
 import { makeCreateUserValidation } from "../validators";
 import { CreateUser } from "@/data/usecases";
+import { BcryptAdapter } from "@/infra/criptography/bcrypt/bcrypt-adapter";
 
 export const makeCreateUserController = (): Controller => {
   const findUserByEmailRepository = new FindUserByEmailPostgresRepository();
   const createUserRepository = new CreateUserPostgresRepository();
   const validation = makeCreateUserValidation()
-  const createUserUseCase = new CreateUser(findUserByEmailRepository, createUserRepository)
+  const salt = 11
+  const hasherAdapter = new BcryptAdapter(salt);
+  const createUserUseCase = new CreateUser(findUserByEmailRepository, hasherAdapter, createUserRepository)
   return new CreateUserController(validation, createUserUseCase)
 }
