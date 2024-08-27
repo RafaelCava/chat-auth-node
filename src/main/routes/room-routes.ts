5226aed7b0005f7c4372daf285f2daebc5964559
Rafael Cavalcante <ra.facavalcante@hotmail.com>
2024-08-27 20:33:56 -0300

    feat: create GET /rooms

--- src/main/routes/room-routes.ts
+++ src/main/routes/room-routes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express'
 import { adaptRoute } from '@/main/adapters'
-import { makeCreateRoomController } from '@/main/factories/controllers'
+import { makeCreateRoomController, makeListAllRoomsController } from '@/main/factories/controllers'
 import { auth } from '../middlewares'
 
 export default (router: Router): void => {
   router.post('/rooms', auth, adaptRoute(makeCreateRoomController()))
+  router.get('/rooms', auth, adaptRoute(makeListAllRoomsController()))
 }
