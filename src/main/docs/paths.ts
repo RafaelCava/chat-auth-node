168a5eaf6d19333e917c67a28164b847ab762ae6
Rafael Cavalcante <ra.facavalcante@hotmail.com>
2024-08-27 23:56:54 -0300

    docs: create GET /auth/profile documentation

--- src/main/docs/paths.ts
+++ src/main/docs/paths.ts
@@ -1,8 +1,9 @@
-import { loginPath, roomsPath, usersPath, refreshPath } from "./paths/";
+import { loginPath, roomsPath, usersPath, refreshPath, profilePath } from "./paths/";
 
 export default {
   '/auth/login': loginPath,
   '/auth/refresh': refreshPath,
+  '/auth/profile': profilePath,
   '/rooms': roomsPath,
   '/users': usersPath,
 }
---
+++ src/main/docs/paths/profile-path.ts
@@ -0,0 +1,30 @@
+export const profilePath = {
+  get: {
+    security: [{
+      apiKeyAuth: []
+    }],
+    tags: ['Auth'],
+    summary: 'API para consultar o perfil do usuário',
+    responses: {
+      200: {
+        description: 'Sucesso',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/user-without-password'
+            }
+          }
+        }
+      },
+      400: {
+        $ref: '#/components/badRequest'
+      },
+      403: {
+        $ref: '#/components/forbidden'
+      },
+      500: {
+        $ref: '#/components/serverError'
+      }
+    }
+  }
+}
