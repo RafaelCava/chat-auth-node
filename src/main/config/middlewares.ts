55cfdb30f7dde83134fc6da406c86972ec69b99f
Rafael Cavalcante <ra.facavalcante@hotmail.com>
2024-09-01 18:17:54 -0300

    refactor: add config on cors lib

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -4,6 +4,10 @@ import { bodyParser, contentType } from '../middlewares'
 
 export default (app: Express): void => {
   app.use(bodyParser)
-  app.use(cors())
+  app.use(cors({
+    origin: '*',
+    allowedHeaders: '*',
+    methods: '*'
+  }))
   app.use(contentType)
 }
